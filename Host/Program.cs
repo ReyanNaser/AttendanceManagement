6973228307096c23e11f5499c2ccbb3a04398a0c
Reyan Naser <reyan.naser12@gmail.com>
2026-01-12 16:02:48 +0500

    removed comment

--- Host/Program.cs
+++ Host/Program.cs
@@ -13,7 +13,7 @@ using static Application.Employee.CreateEmployee;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 builder.Services.AddOpenApi();
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddProblemDetails();
