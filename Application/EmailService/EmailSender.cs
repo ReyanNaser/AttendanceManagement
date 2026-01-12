920c8e85183a10ba94dfb2db84d249d9dad67120
Reyan Naser <reyan.naser12@gmail.com>
2026-01-12 17:31:07 +0500

    sorted strings

--- Application/EmailService/EmailSender.cs
+++ Application/EmailService/EmailSender.cs
@@ -1,12 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
-using Microsoft.IdentityModel.Tokens;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Mail;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Application.EmailService
 {
